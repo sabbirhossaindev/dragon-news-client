13479a97e43069287a7c4b1008513dda1f3d908c
sabbir hossain <mdsabbir477470@gmail.com>
2022-10-18 23:00:25 +0600

    dragon-news-client

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -22,7 +22,7 @@ const RightSideNav = () => {
                     <ListGroup.Item className='mb-2'><FaWhatsapp/> Whatsapp</ListGroup.Item>
                     <ListGroup.Item className='mb-2'><FaTwitter/> Twitter</ListGroup.Item>
                     <ListGroup.Item className='mb-2'><FaTwitch/> Twitch</ListGroup.Item>
-                    <ListGroup.Item className='mb-2'>Vestibulum at eros</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'>at eros</ListGroup.Item>
                 </ListGroup>
             </div>
             <div>
